07d7942650a9eecb793637898736ef7dbffec534
Collilieux Brice <collilieux.brice@gmail.com>
2017-05-03 13:35:51 +0200

    clock with dynamic french time

--- public/js/components/common/navbar.js
+++ public/js/components/common/navbar.js
@@ -1,7 +1,10 @@
+import moment from "moment"
 let navbar = {
     templateUrl: 'js/components/common/navbar.html',
-    controller: ['UsersService', '$state', function(UsersService, $state) {
+    controller: ['UsersService', '$state', '$interval', function(UsersService, $state, $interval) {
         'use strict'
+
+        this.clock = moment();
         angular.extend(this, {
             $onInit() {
                 UsersService.getCurrent().then((user) => {
@@ -18,7 +21,8 @@ let navbar = {
                 })
             }
 
-        })
+        }) 
+        $interval(() => {this.clock = moment().locale('fr').format("dddd d MMM YYYY, HH:m, s")}, 1000)
     }]
 }
 
